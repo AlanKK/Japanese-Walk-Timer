093c784321c471de40e2f6d601920af6d48073c2
Alan Keister <alankeister@gmail.com>
2026-02-20 09:54:11 -0500

    Initial Commit

---
+++ Japanese Walk Watch App/Japanese_WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_WalkApp.swift
+//  Japanese Walk Watch App
+//
+//  Created by Alan K on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Japanese_Walk_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Japanese Walk Watch AppTests/Japanese_Walk_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_Walk_Watch_AppTests.swift
+//  Japanese Walk Watch AppTests
+//
+//  Created by Alan K on 2/20/26.
+//
+
+import Testing
+@testable import Japanese_Walk_Watch_App
+
+struct Japanese_Walk_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
